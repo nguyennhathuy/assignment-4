087d566edf7b1c2136a8023bead0fdf7e4cc25ae
Huy Nguyen <nhathuy120796@gmail.com>
2023-10-12 22:29:10 +0700

    daily commit 4

---
+++ src/components/Header.tsx
@@ -0,0 +1,85 @@
+function Header() {
+  return (
+    <header className="px-2">
+      <div>
+        <span>Bookstore</span>
+      </div>
+      <div className="border border-zinc-300 border-2 flex justify-between items-center bg-white p-2">
+        <div>
+          <h1 className="text-3xl font-bold">Bookstore</h1>
+        </div>
+        <div className="flex gap-7">
+          <div>
+            <label htmlFor="select-mode" className="mr-2 flex items-center">
+              <input
+                type="checkbox"
+                id="select-mode"
+                className="
+                  relative
+                  appearance-none 
+                  w-[40px] 
+                  h-[20px] 
+                  bg-[#C5D0DA] 
+                  rounded-[10px] 
+                  mr-[5px] 
+                  cursor-pointer
+                  checked:bg-[#D2445C]
+                  transition-all
+                  before:absolute
+                  before:content-['']
+                  before:w-5
+                  before:h-5
+                  before:rounded-full
+                  before:bg-white
+                  before:scale-90
+                  before:top-0
+                  before:left-0
+                  before:transition-all
+                  before:checked:left-[20px]
+                "
+              />
+              <span>Dark mode</span>
+            </label>
+          </div>
+          <div className="flex gap-2">
+            <img
+              className="w-[25px] mix-blend-hard-light"
+              src="/user-logo.png"
+              alt="user-logo"
+            />
+            <span>John Doe</span>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
+/*
+<label htmlFor="switch" className="switch-dark-mode">
+            <input type="checkbox" id="switch"/>
+            <span>Dark mode</span>
+        </label>
+input[type="checkbox"]::before {
+    content: '';
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    top: 0;
+    left: 0;
+    background-color: #fff;
+    transform: scale(0.9);
+    transition: .3s
+}
+
+input:checked[type="checkbox"]:before {
+    left: 20px;
+}
+
+.switch-dark-mode {
+    display: flex;
+    align-items: center;
+}
+*/
---
+++ src/styles/enumStyle.ts
@@ -0,0 +1,2 @@
+export const testClass = `text-red-800`
+export const sizeClass = `text-6xl`
