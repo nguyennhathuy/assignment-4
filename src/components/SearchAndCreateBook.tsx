415f54fbf88da773fa95dd10652b95e9fd9db5ac
nguyennhathuy <nhathuy120796@gmail.com>
2023-10-13 22:22:57 +0700

    daily commit 3

---
+++ src/components/SearchAndCreateBook.tsx
@@ -0,0 +1,19 @@
+function SearchAndCreateBook() {
+  return (
+    <div className="flex justify-between">
+      <label htmlFor="search-book">
+        <input
+          type="text"
+          id="search-book"
+          placeholder="Search books"
+          className="border-2 border-zin-300 py-2 px-3 rounded-md"
+        />
+      </label>
+      <button className="py-2 px-3 bg-[#D2445C] text-white rounded-md">
+        Add book
+      </button>
+    </div>
+  )
+}
+
+export default SearchAndCreateBook
---
+++ src/components/Table.tsx
@@ -0,0 +1,44 @@
+import Link from 'next/link'
+import { TABLE_HEADER, DUMMY_DATA } from '../enum'
+
+function Table() {
+  return (
+    <div className="mt-5">
+      <table className="table-fixed w-[100%] border-collapse">
+        <thead>
+          <tr>
+            {Object.keys(TABLE_HEADER).map((item, index) => (
+              <th key={index} className="border-2 border-zin-300 py-1 px-2">
+                {TABLE_HEADER[item]}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {DUMMY_DATA.map((book) => (
+            <tr key={book.id}>
+              <td className="border-2 border-zin-300 py-1 px-2">
+                {book[TABLE_HEADER.NAME]}
+              </td>
+              <td className="border-2 border-zin-300 py-1 px-2">
+                {book[TABLE_HEADER.AUTHOR]}
+              </td>
+              <td className="border-2 border-zin-300 py-1 px-2">
+                {book[TABLE_HEADER.TOPIC]}
+              </td>
+              <td className="border-2 border-zin-300 py-1 px-2 flex justify-center gap-2">
+                <button className="py-1 px-2 bg-green-500 text-white rounded-md">
+                  <Link href={`/book/${book.id}`}>Detail</Link>
+                </button>
+                <button className="py-1 px-2 bg-gray-400 text-white rounded-md">
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+export default Table
