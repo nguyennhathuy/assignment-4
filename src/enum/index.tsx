43339370bcc6c901b852b2c7dfff70e70b0ef29c
nguyennhathuy <nhathuy120796@gmail.com>
2023-10-13 22:23:03 +0700

    daily commit 4

---
+++ src/enum/index.tsx
@@ -0,0 +1,40 @@
+export const TABLE_HEADER = {
+  NAME: 'NAME',
+  AUTHOR: 'AUTHOR',
+  TOPIC: 'TOPIC',
+  ACTION: 'ACTION',
+}
+
+export const DUMMY_DATA = [
+  {
+    id: idGenerator(),
+    [TABLE_HEADER.NAME]: 'Alfreds Futterkiste',
+    [TABLE_HEADER.AUTHOR]: 'Maria Anders',
+    [TABLE_HEADER.TOPIC]: 'Maria Anders',
+  },
+  {
+    id: idGenerator(),
+    [TABLE_HEADER.NAME]: 'Alfreds Futterkiste',
+    [TABLE_HEADER.AUTHOR]: 'Maria Anders',
+    [TABLE_HEADER.TOPIC]: 'Maria Anders',
+  },
+  {
+    id: idGenerator(),
+    [TABLE_HEADER.NAME]: 'Alfreds Futterkiste',
+    [TABLE_HEADER.AUTHOR]: 'Maria Anders',
+    [TABLE_HEADER.TOPIC]: 'Maria Anders',
+  },
+  {
+    id: idGenerator(),
+    [TABLE_HEADER.NAME]: 'Alfreds Futterkiste',
+    [TABLE_HEADER.AUTHOR]: 'Maria Anders',
+    [TABLE_HEADER.TOPIC]: 'Maria Anders',
+  },
+]
+
+export function idGenerator(): string {
+  const S4 = function (): string {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1)
+  }
+  return `${S4()}${S4()}-${S4()}-${S4()}-${S4()}-${S4()}${S4()}${S4()}`
+}
