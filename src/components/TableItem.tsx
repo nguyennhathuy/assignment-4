70c889905bb17c21aee94f2c6ea832be9cadc25d
Huy Nguyen <nhathuy120796@gmail.com>
2023-10-14 19:53:51 +0700

    daily commit 4

---
+++ src/components/TableItem.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link'
+import { DUMMY_DATA, TABLE_HEADER } from '../enum'
+
+type Props = {
+  handleToggleDelteModal: () => void
+}
+
+function TableItem({ handleToggleDelteModal }: Props) {
+  return (
+    <>
+      {DUMMY_DATA.map((book) => (
+        <tr key={book.id}>
+          <td className="border-2 border-zin-300 py-1 px-2">
+            {book[TABLE_HEADER.NAME.toLocaleLowerCase()]}
+          </td>
+          <td className="border-2 border-zin-300 py-1 px-2">
+            {book[TABLE_HEADER.AUTHOR.toLocaleLowerCase()]}
+          </td>
+          <td className="border-2 border-zin-300 py-1 px-2">
+            {book[TABLE_HEADER.TOPIC.toLocaleLowerCase()]}
+          </td>
+          <td className="border-2 border-zin-300 py-1 px-2 flex justify-center gap-2">
+            <button className="py-1 px-2 bg-green-500 text-white rounded-md">
+              <Link href="/book/1">Detail</Link>
+            </button>
+            <button
+              onClick={handleToggleDelteModal}
+              className="py-1 px-2 bg-gray-400 text-white rounded-md"
+            >
+              Delete
+            </button>
+          </td>
+        </tr>
+      ))}
+    </>
+  )
+}
+
+export default TableItem
