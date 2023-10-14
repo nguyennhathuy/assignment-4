d59eacaca3dfb71cfb874522d2d67efcc21154e0
Huy Nguyen <nhathuy120796@gmail.com>
2023-10-14 19:53:36 +0700

    daily commit 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,6 @@
 import React from 'react'
-import Table from '../components/Table'
-import SearchAndCreateBook from '../components/SearchAndCreateBook'
-import DeleteModal from '../components/DeleteModal'
-import CreateModal from '../components/CreateModal'
+import BodyContent from '../components/BodyContent'
 
 export default function Home() {
-  return (
-    <>
-      {/* <SearchAndCreateBook />
-      <Table /> */}
-      {/* <DeleteModal /> */}
-      <CreateModal />
-    </>
-  )
+  return <BodyContent />
 }
--- src/components/CreateModal.tsx
+++ src/components/CreateModal.tsx
@@ -1,15 +1,23 @@
-import { TABLE_HEADER } from '../enum'
+import Image from 'next/image'
+import { TABLE_HEADER, TOPIC } from '../enum'
 
-function CreateModal() {
+type Props = {
+  handleToggleCreateModal: () => void
+}
+
+function CreateModal({ handleToggleCreateModal }: Props) {
   return (
     <div className="bg-black/50 fixed top-0 left-0 h-screen w-[100%]">
       <div className="bg-white w-[300px] relative top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] p-4">
         <div className="flex justify-between">
           <span className="font-bold">Add book</span>
-          <img
-            className="w-[25px] mix-blend-hard-light cursor-pointer"
+          <Image
+            className="mix-blend-hard-light cursor-pointer"
             src="/close-icon.png"
             alt="user-logo"
+            width={25}
+            height={25}
+            onClick={handleToggleCreateModal}
           />
         </div>
         <div className="flex flex-col my-5">
@@ -26,20 +34,22 @@ function CreateModal() {
                 <input
                   id={item.toLocaleLowerCase()}
                   type="text"
-                  className="border-2 border-zin-300 rounded-md w-full"
+                  className="border-2 border-zin-300 rounded-md w-full pl-2"
                 />
               ) : (
                 <select
                   id="topic"
-                  className="border-2 border-zin-300 rounded-md w-full"
+                  className="border-2 border-zin-300 rounded-md w-full pl-2"
+                  value=""
                 >
-                  <option value="" disabled selected hidden>
+                  <option value="" disabled hidden>
                     Select a topic
                   </option>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
+                  {TOPIC.map((topic, index) => (
+                    <option key={index} value={topic}>
+                      {topic}
+                    </option>
+                  ))}
                 </select>
               )}
             </label>
@@ -49,7 +59,10 @@ function CreateModal() {
           <button className="py-2 px-6 bg-green-500 text-white rounded-md">
             Create
           </button>
-          <button className="py-2 px-6 bg-gray-400 text-white rounded-md">
+          <button
+            className="py-2 px-6 bg-gray-400 text-white rounded-md"
+            onClick={handleToggleCreateModal}
+          >
             Cancel
           </button>
         </div>
