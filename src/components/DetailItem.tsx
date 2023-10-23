04b1189743a1084cd9c24696684d4b0871daffb3
Huy Nguyen <nhathuy120796@gmail.com>
2023-10-23 21:24:52 +0700

    daily commit 2

--- src/components/DetailItem.tsx
+++ src/components/DetailItem.tsx
@@ -1,9 +1,41 @@
+'use client'
+
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+import { Book } from '../types'
+
 type Props = {
   id: string
 }
 
 function DetailItem({ id }: Props): JSX.Element {
-  return <h1>{id}</h1>
+  const [specificBook, setSpecificBook] = useState<Book | null>()
+  useEffect(() => {
+    const allBooks: Book[] = JSON.parse(
+      localStorage.getItem('bookList') || '[]',
+    )
+    const specificBook: Book | undefined = allBooks.find(
+      (item) => item.id === id,
+    )
+    if (specificBook) setSpecificBook(specificBook)
+  }, [])
+  return (
+    <>
+      <Link href="/" className="text-red-500">
+        {`<  Back`}
+      </Link>
+      <div className="my-6">
+        <h1 className="mb-6 font-bold text-2xl">{specificBook?.name}</h1>
+        <p>
+          <span className="font-semibold">Author:</span> {specificBook?.author}
+        </p>
+        <p>
+          <span className="font-semibold">Topic:</span> {specificBook?.topic}
+        </p>
+      </div>
+      <button className="text-red-500 border-b border-red-400">Delete</button>
+    </>
+  )
 }
 
 export default DetailItem
